2699c930f22247c5103fe9407552b601b8e80237
aleksandr <beznutrovs@gmail.com>
2025-10-03 12:47:56 +0500

    задание 1 выполнено

--- src/main/java/ru/naumen/collection/task1/Ticket.java
+++ src/main/java/ru/naumen/collection/task1/Ticket.java
@@ -1,5 +1,7 @@
 package ru.naumen.collection.task1;
 
+import java.util.Objects;
+
 /**
  * Билет
  *
@@ -9,4 +11,17 @@ package ru.naumen.collection.task1;
 public class Ticket {
     private long id;
     private String client;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return id == ticket.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
